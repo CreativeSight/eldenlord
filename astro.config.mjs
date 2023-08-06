d99066f8894157429f620e898d7f2a8ea7db214d
konradlesniak <10002647@ergo.com>
2023-08-06 17:27:48 +0200

    Astro config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://creativesight.github.io",
+  base: "/eldenlord",
   integrations: [tailwind()],
 });
